b4162eff24b899c68b10e97697982c2ce749fd4b
anuragsarma33@gmail.com <anuragsarma33@gmail.com>
2022-03-19 19:10:55 +0530

    Added cors to fix cors error

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 const app = express();
 
-app.use(express.json({extended: false}))
+app.use(express.json({ extended: false }));
+
+//cross origin resource sharing
+app.use(cors());
 
 //databse connection
 connectDB();
-//routes
-app.use('/',require('./routes'));
 
-app.listen(5000,() => console.log('Server is running'));
+//routes
+app.use("/", require("./routes"));
+
+app.listen(5000, () => console.log("Server is running"));
